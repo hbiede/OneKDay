49f82952aede40498693eb77f44f12262212563d
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2022-11-08 21:50:43 -0600

    Initial Commit

---
+++ OneKDay/OneKDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneKDayApp.swift
+//  OneKDay
+//
+//  Created by Elijah Biede on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct OneKDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
