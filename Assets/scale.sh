e6d16b6b62276bbcf613dc0ac4f8fc7a9acfccb1
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2022-11-13 17:27:31 -0600

    Add Candy icon

--- Assets/scale.sh
+++ Assets/scale.sh
@@ -2,7 +2,7 @@ for image in *.png;
 do
     filename=$(basename -- "$image");
     filename="${filename%.*}";
-    ffmpeg -i $image -vf scale=60:60 "$image-Preview.png";
-    ffmpeg -i $image -vf scale=120:120 "$image-Preview@2x.png";
-    ffmpeg -i $image -vf scale=180:180 "$image-Preview@3x.png";
+    ffmpeg -i $image -vf scale=60:60 "$filename-Preview.png";
+    ffmpeg -i $image -vf scale=120:120 "$filename-Preview@2x.png";
+    ffmpeg -i $image -vf scale=180:180 "$filename-Preview@3x.png";
 done
